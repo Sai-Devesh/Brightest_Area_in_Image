17f70b3e3082d3aa94ba16cd05039d2aba3b6c55
Sai Devesh <129711766+Sai-Devesh@users.noreply.github.com>
2024-07-14 23:00:47 +0530

    Update Brightest_area.cpp

--- Brightest_area.cpp
+++ Brightest_area.cpp
@@ -1,11 +1,6 @@
 /*
 Author: Annepu Sai Devesh
 Date: March 11 2024
-Given a text file containing a rectangular matrix representing the pixel values in an image 
-(in the range 0 to 255). Designing an algorithm to print the brightest area in the image. 
-The definition of the brightest area is a rectangular submatrix of A[][] of maximum sum, 
-where A[][] is obtained by subtracting 128 from each pixel value in the original image. 
-Also used Kadane's algorithm as a subroutine to find the brightest area.
 */
 
 #include <stdio.h>
